931bf9ea126c58c505822ee5bb7d5d5a10e57a78
David Cumps <david@cumps.be>
2020-07-07 16:40:20 +0200

    chore: add missing using

--- src/Be.Vlaanderen.Basisregisters.Converters.Timestamp/TimestampConverter.cs
+++ src/Be.Vlaanderen.Basisregisters.Converters.Timestamp/TimestampConverter.cs
@@ -1,5 +1,6 @@
 namespace Be.Vlaanderen.Basisregisters.Converters.Timestamp
 {
+    using System.Globalization;
     using Newtonsoft.Json.Converters;
 
     public class TimestampConverter : IsoDateTimeConverter
